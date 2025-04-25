61b391a368e894f08ddf930f304e58460929de12
LizethGarcia89 <lizt15_816@hotmail.com>
2025-04-25 01:12:39 -0500

    Add files via upload

--- Model_deployment_LOC/app.py
+++ Model_deployment_LOC/app.py
@@ -45,3 +45,6 @@ class PopularityApi(Resource):
         features = np.array([list(data.values())]).astype(float)
         prediction = model.predict(features)[0]
         return {'Resultado Predicción': f'{prediction:.2f}'}, 200
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
